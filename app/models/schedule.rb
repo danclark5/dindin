2b8b92ae5770f1f981f108af9feaedf9b91aca36
Dan Clark <wpu.clark@gmail.com>
2019-11-05 07:29:35 -0500

    Add #meal_for to Schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,13 @@ class Schedule < ApplicationRecord
   validates :start_date, :end_date, :default_participant_count, presence: true
   validates_inclusion_of :include_breakfast, :include_lunch, :include_dinner, in: [true, false]
 
+  has_many :scheduled_meals
+
   def included_dates
     (self.start_date..self.end_date).to_a
+  end
+
+  def meal_for date, meal_type
+    self.scheduled_meals.select { |scheduled_meal| scheduled_meal.date == date && scheduled_meal.meal_type == meal_type }.first
   end
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
 
-  subject(:schedule) { build(:schedule) }
+  subject(:schedule) { create(:scheduled_meal).schedule }
 
   describe "#included_dates" do
 
@@ -16,6 +16,20 @@ RSpec.describe Schedule, type: :model do
       subject(:schedule) { build(:schedule, start_date: 6.days.from_now, end_date: 3.days.from_now) }
       it "returns 0 dates" do
         expect(schedule.included_dates.length).to eq(0)
+      end
+    end
+  end
+  describe "#meal_for" do
+    context "for a period that has a meal scheduled" do
+      let(:scheduled_meal) { schedule.scheduled_meals.first }
+      it "returns that scheduled meal" do
+        expect(schedule.meal_for(schedule.start_date, scheduled_meal.meal_type)).to eq(scheduled_meal)
+      end
+    end
+    context "for a period that does not have a meal scheduled" do
+      let(:scheduled_meal) { schedule.scheduled_meals.first }
+      it "returns nil" do
+        expect(schedule.meal_for(schedule.start_date + 1.day, scheduled_meal.meal_type)).to be_nil
       end
     end
   end
