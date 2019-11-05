546dc8ea9c1fb8980745db8769c86968d35c93ca
Dan Clark <wpu.clark@gmail.com>
2019-11-05 06:14:53 -0500

    Add included_dates method to schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,8 @@
 class Schedule < ApplicationRecord
   validates :start_date, :end_date, :default_participant_count, presence: true
   validates_inclusion_of :include_breakfast, :include_lunch, :include_dinner, in: [true, false]
+
+  def included_dates
+    (self.start_date..self.end_date).to_a
+  end
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject(:schedule) { build(:schedule) }
+
+  describe "#included_dates" do
+
+    context "schedule has a set of valid start and end dates" do
+      it "returns 7 dates" do
+        expect(schedule.included_dates.length).to eq(7)
+      end
+    end
+
+    context "schedule has a start date that is after the end date" do
+      subject(:schedule) { build(:schedule, start_date: 6.days.from_now, end_date: 3.days.from_now) }
+      it "returns 0 dates" do
+        expect(schedule.included_dates.length).to eq(0)
+      end
+    end
+  end
 end
