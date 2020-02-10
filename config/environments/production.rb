c5e320fb2a59c57e3d8dbcaeb4ae1747c34a4d92
Dan Clark <wpu.clark@gmail.com>
2020-02-09 20:44:23 -0500

    other stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,12 +95,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'yourdomain.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    from:           'dan@dnnrpckr.com',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password:  ENV['SENDGRID_PASSWORD'],
+    domain: 'dnnrpckr.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
