a790f5630d13145572f7c77961eaae2a3e13488a
Dan Clark <wpu.clark@gmail.com>
2020-03-06 10:24:40 -0500

    Rollback multisearch migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_131149) do
+ActiveRecord::Schema.define(version: 2020_03_06_125718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,15 +44,6 @@ ActiveRecord::Schema.define(version: 2020_03_06_131149) do
     t.bigint "meal_id"
     t.index ["meal_id"], name: "index_meals_users_on_meal_id"
     t.index ["user_id"], name: "index_meals_users_on_user_id"
-  end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "posts", force: :cascade do |t|
