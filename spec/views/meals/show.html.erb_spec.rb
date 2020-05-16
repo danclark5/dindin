11b60a7c75b40626935fbca08f2b0c1b11d966e2
Dan Clark <wpu.clark@gmail.com>
2020-05-16 16:52:10 -0400

    get a baseline

--- spec/views/meals/show.html.erb_spec.rb
+++ spec/views/meals/show.html.erb_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "meals/show", type: :view do
   before(:each) do
+    sign_in create(:user, :admin)
+    create(:tag, name: 'Chicken')
+    assign(:tags, Tag.all)
     @meal = assign(:meal, create(:meal, name: 'Pizza'))
   end
 
