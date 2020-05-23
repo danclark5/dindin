f70e681f8584623f3c9c9637c45c966e4510c51c
Dan Clark <wpu.clark@gmail.com>
2020-05-23 09:13:02 -0400

    Remove jquery gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery-ui/widgets/autocomplete
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
