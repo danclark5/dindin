4c41065fbf3f3dac69eb3660994db0ba9209bfd9
Dan Clark <wpu.clark@gmail.com>
2020-07-03 06:31:28 -0400

    Missed some stuff

---
+++ app/reflexes/meal_reflex.rb
@@ -0,0 +1,8 @@
+class MealReflex < StimulusReflex::Reflex
+  delegate :current_user, to: :connection
+  def attach_suggested_meal 
+    @scheduled_meal = ScheduledMeal.new(meal_id: element.dataset[:meal_id].to_i, date: element.dataset[:date])
+    @scheduled_meal.user = current_user
+    @scheduled_meal.save
+  end
+end
