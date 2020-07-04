96824ccb17832bd816834383f744f63565d81f26
Dan Clark <wpu.clark@gmail.com>
2020-07-03 22:53:06 -0400

    add a refresh to suggested meal

--- app/reflexes/meal_reflex.rb
+++ app/reflexes/meal_reflex.rb
@@ -1,8 +1,11 @@
 class MealReflex < StimulusReflex::Reflex
   delegate :current_user, to: :connection
-  def attach_suggested_meal 
+  def attach_suggested_meal
     @scheduled_meal = ScheduledMeal.new(meal_id: element.dataset[:meal_id].to_i, date: element.dataset[:date])
     @scheduled_meal.user = current_user
     @scheduled_meal.save
+  end
+
+  def reload_suggested_meal
   end
 end
