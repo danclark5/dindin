86bbe90f5a5e7b3ad931b985508d0a3559966f0c
Dan Clark <wpu.clark@gmail.com>
2020-08-16 08:31:20 -0400

    fix autocomplete...maybe

--- app/javascript/packs/scheduled_meals.js
+++ app/javascript/packs/scheduled_meals.js
@@ -9,20 +9,25 @@ document.addEventListener("turbolinks:load", function() {
     }
     return "<div class=\"meal_results\">" + meal.label + tag  + "</div>";
   }
+
   $('#meal').autocomplete({
     source: $('#meal').data('autocomplete-source'),
     select: function(_, ui) {
-      $('#scheduled_meal_meal_id').val(ui.item.value)
-      $('#meal').val(ui.item.label)
-      return false
+      $('#scheduled_meal_meal_id').val(ui.item.value);
+      $('#meal').val(ui.item.label);
+      return false;
     },
     minLength: 0
-  });
-  $('#meal').focus( function() {
-    $(this).autocomplete("search",$(this).val())
-  }).autocomplete("instance")._renderItem = function(ul,meal) {
+  })
+  .autocomplete("instance")._renderItem = function(ul,meal) {
     return $( "<li>" )
       .append( build_meal_result(meal) )
       .appendTo( ul );
-  }
+  };
+
+  $('#meal').focus( function() {
+    if (!$(this).val()) {
+      $(this).autocomplete("search",$(this).val())
+    }
+  })
 });
