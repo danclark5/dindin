e5e29ad2b2aafa4c3208b257c65dda3b998fb72a
Dan Clark <wpu.clark@gmail.com>
2020-09-25 06:06:51 -0400

    fix shopping list update decision

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -2,12 +2,12 @@ class ShoppingListsController < ApplicationController
   def show
     @shopping_list_items = ShoppingListItem.items_for(current_user)
     @is_shopping_list_present = @shopping_list_items.count > 0
-    @is_shopping_list_current = covered_scheduled_meals == current_scheduled_meals
+    @is_shopping_list_current = (covered_scheduled_meals & current_scheduled_meals).sort == current_scheduled_meals.sort
   end
 
   private
   def covered_scheduled_meals
-    (@shopping_list_items.map { |sli| sli.scheduled_meal_id }).uniq
+    @shopping_list_items.map { |sli| sli.scheduled_meal_id }
   end
 
   def current_scheduled_meals
