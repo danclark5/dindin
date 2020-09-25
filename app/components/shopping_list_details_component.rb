9fc8cd810400375414645e0e3d4e157bb7993693
Dan Clark <wpu.clark@gmail.com>
2020-09-25 07:06:13 -0400

    Init Add item

--- app/components/shopping_list_details_component.rb
+++ app/components/shopping_list_details_component.rb
@@ -3,4 +3,19 @@ class ShoppingListDetailsComponent < ViewComponentReflex::Component
     @user = user
     @shopping_list_items = ShoppingListItem.items_for(@user).order(:acquired)
   end
+
+  def add_ingredient
+    if element.dataset[:ingredient_id] != "0"
+      ingredient = Ingredient.find(element.dataset[:ingredient_id])
+    else
+      ingredient = Ingredient.create(name: element.dataset[:ingredient_term].titleize)
+      if @user.user_type != 'admin'
+        ingredient.user = @user
+        ingredient.save
+      end
+    end
+    ShoppingListItem.create(ingredient: ingredient, user: @user, acquired: false) if ingredient
+    refresh! '.shopping-list-details', selector
+    refresh! '#shopping-list-header', selector
+  end
 end
