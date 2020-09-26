f398adce31f0ea17662992cb6872c0b9fd16c8c6
Dan Clark <wpu.clark@gmail.com>
2020-09-26 09:45:57 -0400

    Actually cancel a cancel or remove on the shopping list.

---
+++ app/javascript/controllers/shopping_lists_controller.js
@@ -0,0 +1,16 @@
+import ApplicationController from './application_controller'
+import { Controller } from 'stimulus'
+import StimulusReflex from 'stimulus_reflex'
+
+export default class extends ApplicationController {
+
+  clear_shopping_list(event, response) {
+    event.preventDefault()
+    if (event["detail"][0]) this.stimulate('ShoppingListHeaderComponent#clear_shopping_list', event.target)
+  }
+
+  remove_shopping_list_item(event, response) {
+    event.preventDefault()
+    if (event["detail"][0]) this.stimulate('ShoppingListItemComponent#delete', event.target)
+  }
+}
