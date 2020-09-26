8db5e75fe10fc5aaae5d6c9e9192bed91a6baa93
Dan Clark <wpu.clark@gmail.com>
2020-09-26 13:00:12 -0400

    shopping list ordering

--- app/components/shopping_list_details_component.rb
+++ app/components/shopping_list_details_component.rb
@@ -1,7 +1,7 @@
 class ShoppingListDetailsComponent < ViewComponentReflex::Component
   def initialize(user)
     @user = user
-    @shopping_list_items = ShoppingListItem.items_for(@user).order(:acquired)
+    @shopping_list_items = ShoppingListItem.items_for(@user).includes(:ingredient).includes(:scheduled_meal).order("ingredients.ingredient_category_id, ingredients.name, acquired")
   end
 
   def add_ingredient
