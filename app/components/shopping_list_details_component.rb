ac3042d0882b40ae9fa0c3e3dd1ed0b2777d2aca
Dan Clark <wpu.clark@gmail.com>
2020-10-12 07:49:56 -0400

    Move the add ingredient field

--- app/components/shopping_list_details_component.rb
+++ app/components/shopping_list_details_component.rb
@@ -4,22 +4,4 @@ class ShoppingListDetailsComponent < ViewComponentReflex::Component
     @shopping_list_items = ShoppingListItem.items_for(@user).includes(:ingredient).includes(:scheduled_meal).order("ingredients.ingredient_category_id, acquired, ingredients.name")
   end
 
-  def add_ingredient
-
-    if element.dataset[:ingredient_id] != "0"
-      ingredient = Ingredient.find(element.dataset[:ingredient_id])
-    elsif element.dataset[:ingredient_term].length > 0
-      if @user.user_type != 'admin'
-        user_id = @user.id
-      else
-        user_id = nil
-      end
-      ingredient = Ingredient.find_or_create_by(name: element.dataset[:ingredient_term].titleize,
-                                             ingredient_category_id: USER_ITEM.id,
-                                             user_id: user_id)
-    end
-    ShoppingListItem.create(ingredient: ingredient, user: @user, acquired: false) if ingredient
-    refresh! '.shopping-list-details', selector
-    refresh! '#shopping-list-header', selector
-  end
 end
--- app/components/shopping_list_header_component.rb
+++ app/components/shopping_list_header_component.rb
@@ -54,4 +54,23 @@ class ShoppingListHeaderComponent < ViewComponentReflex::Component
     (shopping_list_items.max_by { |sli| sli&.scheduled_meal&.date || Date.today }.scheduled_meal&.date || Date.today) if shopping_list_items.any?
   end
 
+  def add_ingredient
+
+    if element.dataset[:ingredient_id] != "0"
+      ingredient = Ingredient.find(element.dataset[:ingredient_id])
+    elsif element.dataset[:ingredient_term].length > 0
+      if @user.user_type != 'admin'
+        user_id = @user.id
+      else
+        user_id = nil
+      end
+      ingredient = Ingredient.find_or_create_by(name: element.dataset[:ingredient_term].titleize,
+                                             ingredient_category_id: USER_ITEM.id,
+                                             user_id: user_id)
+    end
+    ShoppingListItem.create(ingredient: ingredient, user: @user, acquired: false) if ingredient
+    refresh! '.shopping-list-details', selector
+    refresh! '#shopping-list-header', selector
+  end
+
 end
