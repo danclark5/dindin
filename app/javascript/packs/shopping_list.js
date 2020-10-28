fc06634445cbe243cf28660f26d7f61cbd5f212e
Dan Clark <wpu.clark@gmail.com>
2020-10-28 07:34:08 -0400

    Toggle Snooze visibility

--- app/javascript/packs/shopping_list.js
+++ app/javascript/packs/shopping_list.js
@@ -5,16 +5,28 @@ function toggle_carret(event) {
 }
 
 function toggle_snooze_button(event){
-  console.log(event)
   event.currentTarget.closest(".shopping_item_container").classList.remove("is-hidden");
 }
 
 function unsnooze_all(event){
-  console.log(event)
   const shopping_item_containers = document.querySelectorAll(".shopping_item_container");
-  shopping_item_containers.forEach(function(shopping_item_containers) {
-    shopping_item_containers.classList.remove("is-hidden");
-  })
+  shopping_item_containers.forEach(function(shopping_item_container) {
+    shopping_item_container.classList.remove("is-hidden");
+    shopping_item_container.classList.remove("snoozed");
+  });
+}
+
+function toggle_show_snoozed(event){
+  const snoozed_shopping_item_containers = document.querySelectorAll(".snoozed");
+  snoozed_shopping_item_containers.forEach(function(shopping_item_container) {
+    shopping_item_container.classList.toggle("is-hidden");
+  });
+  toggle_snoozes_button = document.querySelector("#toggle_snoozes_button")
+  if (toggle_snoozes_button.innerHTML == "Show Snoozed") {
+    toggle_snoozes_button.innerHTML = "Hide Snoozed";
+  } else {
+    toggle_snoozes_button.innerHTML = "Show Snoozed";
+  }
 }
 
 function link_event_listeners(event) {
@@ -29,12 +41,20 @@ function link_event_listeners(event) {
   const snooze_buttons = document.querySelectorAll('.snooze_button');
   if (snooze_buttons.length > 0) {
     snooze_buttons.forEach(function(snooze_button) {
-      snooze_button.removeEventListener("click",  toggle_snooze_button)
-      snooze_button.addEventListener("click",  toggle_snooze_button)
+      snooze_button.removeEventListener("click",  toggle_snooze_button);
+      snooze_button.addEventListener("click",  toggle_snooze_button);
     });
     const reset_snoozes_button = document.querySelector("#reset_snoozes_button");
-    reset_snoozes_button.removeEventListener("click", unsnooze_all)
-    reset_snoozes_button.addEventListener("click", unsnooze_all)
+    reset_snoozes_button.removeEventListener("click", unsnooze_all);
+    reset_snoozes_button.addEventListener("click", unsnooze_all);
+  }
+
+  const toggle_snoozes_buttons = document.querySelectorAll('#toggle_snoozes_button');
+  if (toggle_snoozes_buttons.length > 0) {
+    toggle_snoozes_buttons.forEach(function(toggle_snoozes_button) {
+      toggle_snoozes_button.removeEventListener("click", toggle_show_snoozed);
+      toggle_snoozes_button.addEventListener("click", toggle_show_snoozed);
+    });
   }
 }
 
@@ -42,9 +62,7 @@ document.addEventListener("turbolinks:load", link_event_listeners);
 
 document.addEventListener('stimulus-reflex:after', event => {
   list = ['ShoppingListItemComponent', 'ShoppingListHeaderComponent'];
-  console.log(event.detail.reflex.split("#")[0]);
   if (list.includes(event.detail.reflex.split("#")[0])){
-    console.log("linked")
     link_event_listeners(event)
   }
 })
