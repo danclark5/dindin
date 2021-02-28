cf642fd8c557661d81e18b320857be3eceb3e164
Dan Clark <wpu.clark@gmail.com>
2021-02-28 14:58:47 -0500

    Switch user to user_id part 1

--- app/components/shopping_list_header_component.rb
+++ app/components/shopping_list_header_component.rb
@@ -2,22 +2,22 @@ class ShoppingListHeaderComponent < ViewComponentReflex::Component
   def initialize(is_shopping_list_present, is_shopping_list_current, user)
     @is_shopping_list_present = is_shopping_list_present
     @is_shopping_list_current = is_shopping_list_current
-    @user = user
+    @user_id = user.id
     @shopping_list_end_date = get_shopping_list_end_date
-    Rails.logger.info("User: #{user}")
+    Rails.logger.info("User1: #{user_id}")
   end
 
   def create_shopping_list
     @is_shopping_list_present = true
     @is_shopping_list_current = true
-    scheduled_meals = ScheduledMeal.scheduled_meals_for_period(Date.today, Date.today+7, @user)
+    scheduled_meals = ScheduledMeal.scheduled_meals_for_period(Date.today, Date.today+7, @user_id)
     scheduled_meals.each do |scheduled_meal|
       if scheduled_meal.meal.ingredients.count > 0
         scheduled_meal.meal.ingredients.each do |ingredient|
-          ShoppingListItem.create(ingredient: ingredient, scheduled_meal: scheduled_meal, user: @user, acquired: false)
+          ShoppingListItem.create(ingredient: ingredient, scheduled_meal: scheduled_meal, user_id: @user_id, acquired: false)
         end
       else
-        ShoppingListItem.create(scheduled_meal: scheduled_meal, user: @user, acquired: false)
+        ShoppingListItem.create(scheduled_meal: scheduled_meal, user_id: @user_id, acquired: false)
       end
     end
     @shopping_list_end_date = get_shopping_list_end_date
@@ -27,17 +27,17 @@ class ShoppingListHeaderComponent < ViewComponentReflex::Component
   def update_shopping_list
     @is_shopping_list_present = true
     @is_shopping_list_current = true
-    shopping_list_items = ShoppingListItem.items_for(@user)
+    shopping_list_items = ShoppingListItem.items_for(@user_id)
     covered_scheduled_meal_ids = (shopping_list_items.map { |sli| sli.scheduled_meal_id }).uniq
-    scheduled_meals = ScheduledMeal.scheduled_meals_for_period(Date.today, Date.today+7, @user)
+    scheduled_meals = ScheduledMeal.scheduled_meals_for_period(Date.today, Date.today+7, @user_id)
     scheduled_meals.select { |sm| covered_scheduled_meal_ids.exclude? sm.id }
       .each do |scheduled_meal|
       if scheduled_meal.meal.ingredients.count > 0
         scheduled_meal.meal.ingredients.each do |ingredient|
-          ShoppingListItem.create(ingredient: ingredient, scheduled_meal: scheduled_meal, user: @user, acquired: false)
+          ShoppingListItem.create(ingredient: ingredient, scheduled_meal: scheduled_meal, user_id: @user_id, acquired: false)
         end
       else
-        ShoppingListItem.create(scheduled_meal: scheduled_meal, user: @user, acquired: false)
+        ShoppingListItem.create(scheduled_meal: scheduled_meal, user_id: @user_id, acquired: false)
       end
     end
     @shopping_list_end_date = get_shopping_list_end_date
@@ -45,14 +45,14 @@ class ShoppingListHeaderComponent < ViewComponentReflex::Component
   end
 
   def clear_shopping_list
-    Rails.logger.info("User: #{@user}")
-    ShoppingListItem.items_for(@user).destroy_all
+    Rails.logger.info("User2: #{@user_id}")
+    ShoppingListItem.items_for(@user_id).destroy_all
     @shopping_list_end_date = nil
     refresh! '.shopping-list-details', selector
   end
 
   def get_shopping_list_end_date
-    shopping_list_items = ShoppingListItem.items_for(@user)
+    shopping_list_items = ShoppingListItem.items_for(@user_id)
     (shopping_list_items.max_by { |sli| sli&.scheduled_meal&.date || Date.today }.scheduled_meal&.date || Date.today) if shopping_list_items.any?
   end
 
@@ -60,7 +60,7 @@ class ShoppingListHeaderComponent < ViewComponentReflex::Component
     if element.dataset[:ingredient_id] != "0"
       ingredient = Ingredient.find(element.dataset[:ingredient_id])
     elsif element.dataset[:ingredient_term].length > 0
-      if @user.user_type != 'admin'
+      if User.find(@user_id).user_type != 'admin'
         user_id = @user.id
       else
         user_id = nil
@@ -69,13 +69,13 @@ class ShoppingListHeaderComponent < ViewComponentReflex::Component
                                              ingredient_category_id: USER_ITEM.id,
                                              user_id: user_id)
     end
-    ShoppingListItem.create(ingredient: ingredient, user: @user, acquired: false) if ingredient
+    ShoppingListItem.create(ingredient: ingredient, user_id: @user_id, acquired: false) if ingredient
     refresh! '.shopping-list-details', selector
     refresh! '#shopping-list-header', selector
   end
 
   def reset_snoozes
-    ShoppingListItem.items_for(@user).each do |item|
+    ShoppingListItem.items_for(@user_id).each do |item|
       item.snooze_until = nil
       item.save
     end
--- app/models/shopping_list_item.rb
+++ app/models/shopping_list_item.rb
@@ -3,9 +3,9 @@ class ShoppingListItem < ApplicationRecord
   belongs_to :ingredient, optional: true
   belongs_to :scheduled_meal, optional: true
 
-  scope :items_for, ->(user) {
+  scope :items_for, ->(user_id) {
     left_outer_joins(:user)
-      .where("users.id = ? OR users.id is null", user.id)
+      .where("users.id = ? OR users.id is null", user_id)
   }
 
   
