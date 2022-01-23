41f8c7c70383fb9bf594893621173ff808286ac7
Dan Clark <wpu.clark@gmail.com>
2022-01-23 10:21:32 -0500

    timezones are hard

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -10,7 +10,7 @@ class LandingController < ApplicationController
   private
 
   def relevant_start_date
-    return DateTime.parse(params[:start_date]) if params.include? :start_date
-    DateTime.now.in_time_zone(current_user.preferred_timezone)
+    return Date.parse(params[:start_date]) if params.include? :start_date
+    Time.now.in_time_zone(current_user.preferred_timezone).to_date
   end
 end
